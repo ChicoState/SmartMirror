e97eb4f1dcf4d69b6a71591717ff3b43213f56b5
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2019-04-08 13:04:30 -0700

    Finished Splash Screen functionality. fixes #17 issue.

--- PyQtGUI/config.py
+++ PyQtGUI/config.py
@@ -13,7 +13,10 @@ access_secret = "hD3L07PwkhJ7Xtzs84j0s4GbKZmO3GHuySZDmh9RUNUku"
 
 CLIENT_ID = 'f9e266f166854435a7f9c6461b9ee34d'
 CLIENT_SECRET = '62483b38e8514b32ab6fbcde4d68b12f'
-USERNAME = "contreras.andres19"
+USERNAME = "USERNAME"
 
 #Weather Information
-YOUR_API = "KrnJm3pGAtha40EFim82KLEqvaikzMeS"
+
+#YOUR_API = "KrnJm3pGAtha40EFim82KLEqvaikzMeS"
+#YOUR_API = "eOYiiAjNR0EuRaIGNoxAlXQQLn56cQMb"
+YOUR_API = "XVmXE5NJJKoX17t4EucrFusDJSwYcStk"
